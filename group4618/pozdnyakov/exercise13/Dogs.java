3a8224ce49495f21182e3ee05327d27f4e26979b
Pavel Pozdnyakov <pvplpt@inbox.ru>
2024-01-03 03:00:52 +0500

    added solution of problem 13

---
+++ group4618/pozdnyakov/exercise13/Dogs.java
@@ -0,0 +1,13 @@
+package group4618.pozdnyakov.exercise13;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Dogs extends Pets {
+
+    public Dogs(String name, Date birthday, ArrayList<String> commands) {
+        super(name, birthday, commands);
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ group4618/pozdnyakov/exercise13/Donkeys.java
@@ -0,0 +1,13 @@
+package group4618.pozdnyakov.exercise13;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Donkeys extends PackAnimals {
+
+    public Donkeys(String name, Date birthday, ArrayList<String> commands) {
+        super(name, birthday, commands);
+        // TODO Auto-generated constructor stub
+    }
+
+}
